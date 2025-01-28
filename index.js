0535083dfbc1fa8deb2733870a65155b96460c6c
Gurpreet Gumber <gumber.gurpreet@rwit.io>
2025-01-28 15:01:30 +0530

    Setup mongodb seup

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.listen(3333, ()=>{
     console.log("hello there");
 });
+// Connection string
+const DB_URI = 'mongodb+srv://gumbergurpreet:PThtcCxVdIZolkCt@cluster0.xzgs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Connect to MongoDB
+mongoose.connect(DB_URI)
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch((err) => console.error("MongoDB connection failed", err));
+
+app.get('/', (req, res)=> {
+    res.send("hello i am get request");
+});
+
+
+
