38dc3cff0a5402c67c9a004f73258efecd1fab5a
Gurpreet Gumber <gumber.gurpreet@rwit.io>
2025-01-28 15:14:04 +0530

    Install dotenv package + create .env.example file

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+// Import env file
+require('dotenv').config();
+
+// Env constants
+const DB_CONNECTION = process.env.DB_CONNECTION;
+const PORT = process.env.PORT || 3000;
+
+// Add express
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-app.listen(3333, ()=>{
+app.listen(PORT, ()=>{
     console.log("hello there");
 });
-// Connection string
-const DB_URI = 'mongodb+srv://gumbergurpreet:PThtcCxVdIZolkCt@cluster0.xzgs6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 // Connect to MongoDB
-mongoose.connect(DB_URI)
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch((err) => console.error("MongoDB connection failed", err));
+mongoose.connect(DB_CONNECTION)
+    .then(() => console.log("Connected"))
+    .catch((err) => console.error("Connection failed", err));
 
 app.get('/', (req, res)=> {
     res.send("hello i am get request");
